5b5eca147f34abd6f38d9559e89c5c491a7ec53f
Sheryll Dumapal <sheryllhhs@outlook.com>
2023-08-07 20:51:33 +0200

    login form is created

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,25 +1,41 @@
-import { useContext, useState } from "react";
+import React, {useState} from "react"; 
 import axios from "axios";
 import { Link } from "react-router-dom";
 
-const LoginPage = () => {
+export const LoginPage = () => {
     const [email, setEmail] = useState(""); 
     const [password, setPassword] = useState(""); 
 
-    const handleSubmit = () => {
-        //
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        console.log(email)
     }
     return (
         <>
         <div className="login-container">
-        <h1> test </h1>
+        <h1> Sign in </h1>
              <form onSubmit={handleSubmit} className= "login-form">
-               
-                    <label>
-
-                    </label>
+                    <label> E-mail: </label>
+                    <input
+                    type="email" 
+                    placeholder="youremail@gmail.com"
+                    name="email"
+                    id="email" 
+                    value={email}
+                    onChange={(event) => {setEmail(event.taget.value)}}/>
+                
+                <label>Password:</label>
+                <input
+                type="password"
+                placeholder="*******"
+                id="password"
+                name="password"
+                value={password}
+                onChange={(event) => {setPassword(event.target.value)}} />
+                <button type="submit">Log in</button>
                 </form>
-            
+                <button>Don't have an account? Register here.</button>
+               
         </div>
         </>
     )
