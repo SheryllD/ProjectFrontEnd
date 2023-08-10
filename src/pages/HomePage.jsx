59bc1fd81bcff3f7e7a966d6a414172d7dad943b
Sheryll Dumapal <sheryllhhs@outlook.com>
2023-08-10 23:55:41 +0200

    save

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,40 +1,40 @@
-import React from "react"; 
-import "../App.css"; 
-import {Link, useNavigate} from "react-router-dom"; 
+import React from "react";
+import "../App.css";
+import { Link, useNavigate } from "react-router-dom";
 
 export const HomePage = (props) => {
   return (
     <>
-    <div className="HomepageContainer">
-      <h1>Welcome user</h1>
-      <div>
-        <Link to="/todo">
-        <button> Tasks </button>
-        </Link>
+      <div className="HomepageContainer">
+        <h1>Welcome </h1>
+        <div>
+          <Link to="/todo">
+            <button> Tasks </button>
+          </Link>
+        </div>
+        <div>
+          <Link to="/journal">
+            <button> Journal </button>
+          </Link>
+        </div>
+        <div>
+          <Link to="/energylevel">
+            <button> Energy Level </button>
+          </Link>
+        </div>
+        <div>
+          <Link to="/sleepquality">
+            <button> Sleep Quality </button>
+          </Link>
+        </div>
+        <div>
+          <Link to="/statistic">
+            <button> Dashboard </button>
+          </Link>
+        </div>
       </div>
-      <div>
-        <Link to="/journal">
-        <button> Journal </button>
-        </Link>
-      </div>
-      <div>
-        <Link to="/energylevel">
-        <button> Energy Level </button>
-        </Link>
-      </div>
-      <div>
-        <Link to="/sleepquality">
-        <button> Sleep Quality </button>
-        </Link>
-      </div>
-      <div>
-        <Link to="/statistic">
-        <button> Dashboard </button>
-        </Link>
-      </div>
-    </div>
     </>
-  )
-}
+  );
+};
 
 export default HomePage;
